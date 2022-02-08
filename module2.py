73f567305f7f67995d40b07b6a3fcbdc9ee6e351
courtneymickelsen <lam15020@byui.edu>
2022-02-07 21:28:08 -0700

    FINALLY

--- module2.py
+++ module2.py
@@ -1,18 +1,20 @@
 import pandas as pd
 
 def main():
-    GamePicker().setup()
-    GamePicker().check_games()
+    gp = GamePicker()
+    gp.setup()
+    gp.check_games()
 
 class GamePicker():
 
     def __init__(self):
         self.data = pd.read_csv("cse310-module2/bgg_dataset.csv", decimal='.')
-        self.age = 9999
+        self.age = 0
         self.play_time = 9999
         self.player_count = 4
         self.rating = 0
         self.complexity = 0
+        self.complexity_high = self.complexity + 1
 
         self.name_i = 'name'
         self.age_i = 'min_age'
@@ -34,28 +36,36 @@ class GamePicker():
         self.play_time = int(input("How much time do you have (in minutes)? "))
         self.player_count = int(input("How many people want to play? "))
         self.rating = float(input("What's the lowest user rating you would be okay with (1-10)? "))
-        self.complexity = float(input("What's the most complex you would want it to be? "))
+        self.complexity = float(input("How complex do you want it to be? "))
         print('\n\n')
-    
 
     def check_games(self):
         
-        # filters for each category- line of data at category index compared to user input
-        self.age_filt = (self.data[self.age_i] <= self.age)
-        self.play_time_filt = (self.data[self.play_time_i] <= self.play_time)
-        self.max_player_filt = (self.data[self.max_players_i] >= self.player_count)
-        self.min_player_filt = (self.data[self.min_players_i] <= self.player_count)
-        self.rating_filt = (self.data[self.rating_i] >= self.rating)
-        self.complexity_filt = ((self.data[self.complexity_i] <= (self.complexity + 1)) & (self.data[self.complexity_i] >= (self.complexity - 1)))
-
+        # filters for each category- data column at category index compared to user input
+        self.age_filt = self.data[self.age_i] <= self.age
+        self.play_time_filt = self.data[self.play_time_i] <= self.play_time
+        self.max_player_filt = self.data[self.max_players_i] >= self.player_count
+        self.min_player_filt = self.data[self.min_players_i] <= self.player_count
+        self.rating_filt = self.data[self.rating_i] >= self.rating
+        self.complexity_high_filt = self.data[self.complexity_i] >= (self.complexity - 0.1)
+        self.complexity_low_filt = (self.data[self.complexity_i] <= self.complexity_high)
+        # self.all_filt = (self.data['min_age'] <= self.age) & (self.data[self.play_time_i] <= self.play_time) & (self.data[self.max_players_i] >= self.player_count) & (self.data[self.min_players_i] <= self.player_count) & (self.data[self.rating_i] >= self.rating) & (self.data[self.complexity_i].between(self.complexity, self.complexity_high))
+        
         # apply filters to dataframe
-        games_available = (self.data[self.age_filt & self.play_time_filt & self.max_player_filt & self.min_player_filt & self.complexity_filt])
+        games_available = self.data[self.age_filt & self.play_time_filt & self.max_player_filt & self.min_player_filt & self.rating_filt & self.complexity_high_filt]
+        # self.data = self.data[self.play_time_filt]
+        # self.data = self.data[self.max_player_filt]
+        # self.data = self.data[self.min_player_filt]
+        # self.data = self.data[self.rating_filt]
+        # self.data = self.data[self.complexity_filt]
+        # self.data = self.data[self.age_filt]
 
-        names = (games_available['name'])
+
+        display_info = games_available[['name', 'min_players', 'max_players', 'user_rating', 'play_time', 'complexity']]
 
         # print user's top 5 game recommendations
-        print('Top 5 Recommendations:\n\nID #:\tName:\n')
-        print(names.head())
+        print('Top 5 Recommendations:\n\n')
+        print(display_info.head())
         print("\n\n=========================================\n\n")
 
 if __name__ == '__main__':
